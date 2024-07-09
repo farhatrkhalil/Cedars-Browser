e01feba066f3fe12bfa37d63150a287ffbab4e35
farhatrkhalil <khalilrfarhat297@gmail.com>
2024-07-09 17:01:44 +0300

    v0.2 - Added icons for navigation buttons (back, forward, reload, home). - Implemented SSL indicator to show HTTPS status. - Integrated zoom buttons for adjusting page zoom level.

--- Cedars Browser/cedars_browser_env/Cedars Browser/Cedars_Browser.py
+++ Cedars Browser/cedars_browser_env/Cedars Browser/Cedars_Browser.py
@@ -24,6 +24,10 @@ class MainWindow(QMainWindow):
         self.setCentralWidget(self.browser)
         self.showMaximized()
 
+        current_dir = os.path.dirname(os.path.abspath(__file__))
+        app_icon = QIcon(os.path.join(current_dir, 'icons', 'app_icon.png'))
+        self.setWindowIcon(app_icon)
+
         # Inject custom CSS for scrollbars
         self.browser.page().profile().scripts().insert(self.custom_css_script())
 
@@ -66,14 +70,52 @@ class MainWindow(QMainWindow):
         self.url_bar.clicked.connect(self.url_bar.selectAll)
         navbar.addWidget(self.url_bar)
 
+        # SSL lock icon
+        self.ssl_icon = QLabel()
+        self.ssl_icon.setPixmap(QIcon(os.path.join(current_dir, 'icons', 'ssl.png')).pixmap(20, 20))
+        navbar.addWidget(self.ssl_icon)
+        self.ssl_icon.setVisible(False)
+
+        # Add zoom out button
+        zoom_out_btn = QToolButton(self)
+        zoom_out_btn.setIcon(QIcon(os.path.join(current_dir, 'icons', 'zoom_out.png')))
+        zoom_out_btn.clicked.connect(self.zoom_out)
+        zoom_out_btn.setCursor(Qt.PointingHandCursor)
+        navbar.addWidget(zoom_out_btn)
+
+        # Add zoom in button
+        zoom_in_btn = QToolButton(self)
+        zoom_in_btn.setIcon(QIcon(os.path.join(current_dir, 'icons', 'zoom_in.png')))
+        zoom_in_btn.clicked.connect(self.zoom_in)
+        zoom_in_btn.setCursor(Qt.PointingHandCursor)
+        navbar.addWidget(zoom_in_btn)
+
         self.browser.urlChanged.connect(self.update_url)
         self.urlChanged.connect(self.browser.setUrl)
+        
+        # Add options menu button
+        options_btn = QToolButton(self)
+        options_btn.setIcon(QIcon(os.path.join(current_dir, 'icons', 'options.png')))
+        options_btn.setPopupMode(QToolButton.InstantPopup)
+        options_btn.setCursor(Qt.PointingHandCursor)
+        options_btn.setStyleSheet("QToolButton::menu-indicator { image: none; }")  # Remove dropdown indicator
+        navbar.addWidget(options_btn)
+
+        # Create options menu
+        options_menu = QMenu(self)
+        settings_action = QAction('Settings', self)
+        settings_action.triggered.connect(self.open_settings)
+        options_menu.addAction(settings_action)
+        history_action = QAction('History', self)
+        history_action.triggered.connect(self.open_history)
+        options_menu.addAction(history_action)
+
+        options_btn.setMenu(options_menu)
 
     def custom_css_script(self):
         css_code = """
         ::-webkit-scrollbar {
-            width: 17px;
-            height: 30px;
+            border-radius: 10px;
         }
         ::-webkit-scrollbar-thumb {
             background: #888;
@@ -117,6 +159,31 @@ class MainWindow(QMainWindow):
     @pyqtSlot(QUrl)
     def update_url(self, q):
         self.url_bar.setText(q.toString())
+        self.update_ssl_icon(q)
+
+    def update_ssl_icon(self, url):
+        if url.scheme() == 'https':
+            self.ssl_icon.setVisible(True)
+        else:
+            self.ssl_icon.setVisible(False)
+
+    @pyqtSlot()
+    def zoom_in(self):
+        self.browser.setZoomFactor(self.browser.zoomFactor() + 0.1)
+
+    @pyqtSlot()
+    def zoom_out(self):
+        self.browser.setZoomFactor(self.browser.zoomFactor() - 0.1)
+
+    @pyqtSlot()
+    def open_settings(self):
+        # Placeholder method for opening settings
+        print("Opening Settings")
+
+    @pyqtSlot()
+    def open_history(self):
+        # Placeholder method for opening history
+        print("Opening History")
 
 
 if __name__ == "__main__":
